6a56197ee96b7b4000710a02884282a3c303b631
Aspi100 <aspi.jb.ru@gmail.com>
2025-02-03 11:29:50 +0300

    service layer constructor

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,17 +1,23 @@
 package service
 
 import (
-	"github.com/shopspring/decimal"
 	"github.com/google/uuid"
+	"github.com/shopspring/decimal"
 
-	"github.com/aspirin100/finapi/internal/repository"
+	"github.com/aspirin100/finapi/internal/entity"
 )
 
-type BankManager interface {
-	Deposit(userID uuid.UUID, amount decimal.Decimal) error
-	GetTransactions(userID uuid.UUID) 
+type UserManager interface {
+	UpdateBalance(userID uuid.UUID, amount decimal.Decimal) error
+	GetTransactions(userID uuid.UUID) ([]entity.Transaction, error)
 }
 
 type Service struct {
-	Storage *repository.Repository
+	userManager UserManager
+}
+
+func New(userManager UserManager) *Service {
+	return &Service{
+		userManager: userManager,
+	}
 }
